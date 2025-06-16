f8b2b6eedd2ae063acee29b4ae846246d9f9f838
tbizetde@gmail.com <tbizetde@gmail.com>
2025-06-16 13:27:09 -0300

    methods/cyberia/index

---
+++ content/methods/cyberia/index.ts
@@ -0,0 +1,38 @@
+export default {
+  en: {
+    title: "CYBERIA",
+    descripton: [
+      `            
+      In an action of the distribution and germination process, the
+      celebration called <b>Cyberia</b> was born. A meeting-party project
+      promoted by <i>S.E.E.D</i>.
+      `,
+      `            
+      Organically exploring the encounter of strange and rhythmic
+      frequencies with ephemeral visual exhibitions, Cyberia serves as a
+      dissemination, being able to be the first &ltIRL&gt contact of those
+      captivated to affirm the intention of the seed within themselves,
+      serving as a process of organic propagation of the S.E.E.D,
+      infecting the unwary and serving as a affirmation for those already
+      used to it.
+      `,
+      `            
+      Inside and outside their bodies and minds, a night of CYBERIA always
+      ends in daydreaming.
+      `,
+    ],
+    image: {
+      link: "/methods/cyberia",
+      src: r2Url("images", "cyberia_preview.jpg"),
+    },
+    events: [
+      { label: "#000", link: "/methods/cyberia/000" },
+      { label: "#001", link: "/methods/cyberia/001" },
+      { label: "#002", link: "/methods/cyberia/002" },
+      {
+        label: "Episodes on Radio Venenno",
+        link: "https://veneno.live/shows/cyberia-by-s-e-e-d/",
+      },
+    ],
+  },
+};
--- pages/methods/cyberia/index.vue
+++ pages/methods/cyberia/index.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
-import index from "@/content/methods/index";
+import index from "@/content/methods/cyberia/index";
 
 const content = index.en;
 </script>
+
 <template>
   <div
-    class="w-full h-full flex flex-col items-start justify-center px-6 md:px-16 gap-10 md:gap-28 pt-4 pb-14 md:pt-10 overflow-scroll"
+    class="w-full h-full flex flex-col items-start justify-start px-6 md:px-16 gap-10 pt-4 pb-14 md:pt-10"
   >
-    <div v-for="option in content.options">
-      <h1>
-        <NuxtLink :to="option.url">{{ option.title }}</NuxtLink>
-      </h1>
+    <h1>{{ content.title }}</h1>
+
+    <div class="flex flex-col lg:flex-row w-full gap-10 lg:gap-24">
+      <div class="flex-1 flex flex-col gap-6">
+        <div v-for="paragraph in content.descripton" :key="paragraph">
+          <p v-html="paragraph"></p>
+        </div>
+
+        <div class="flex flex-row flex-wrap gap-4">
+          <a
+            v-for="event in content.events"
+            :href="event.link"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-xl font-bold text-black bg-white px-2"
+          >
+            {{ event.label }}
+          </a>
+        </div>
+      </div>
+
+      <div class="flex-1 flex flex-col items-start gap-2">
+        <a
+          :href="content.image.link"
+          class="border-4 border-white"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img :src="content.image.src" class="w-full max-w-[900px]" />
+        </a>
+      </div>
     </div>
   </div>
 </template>
