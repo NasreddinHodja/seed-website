1cd60cfe6f1ee4ff314eff5e95921143c1d47bce
tbizetde@gmail.com <tbizetde@gmail.com>
2025-06-15 23:46:53 -0300

    website link thumbnail preview image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,18 @@
+<sript setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) =>
+    titleChunk ? `${titleChunk} - My Site` : "My Site",
+  meta: [
+    { property: "og:title", content: "S.E.E.D." },
+    { property: "og:description", content: "S.E.E.D. art collective" },
+    { property: "og:image", content: r2Url("images", "seed.png") },
+    { property: "og:type", content: "website" },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:image", content: r2Url("images", "seed.png") },
+  ],
+});
+</sript>
+
 <template>
   <div class="fixed inset-0 -z-50 bg-black"></div>
   <div
