f2ea6a6ec7ffd8b45d24466a04dda7d1985ee659
tbizetde@gmail.com <tbizetde@gmail.com>
2025-06-15 23:00:17 -0300

    web3/popnftnyc2023

---
+++ content/web3/popnftnyc2023.ts
@@ -0,0 +1,42 @@
+export default {
+  en: {
+    title:
+      "S.E.E.D. Collective Exibition on Refraciton x Proof of People Festival at Zero Space NYC - as part of NFT NYC 2023",
+    video: "https://www.youtube.com/embed/bsqFDRoNSIA",
+    pieces: [
+      {
+        link: "https://zora.co/collect/0xd5160927bfe934f65e349abb3a92cc49871ab9e1",
+        label: "noon parallelism",
+      },
+      {
+        link: "https://zora.co/collect/0xAb91E7ee802584aD68c23eCFAcF5824bEF730FDd/1",
+        label: "xhrzd underground lab",
+      },
+      {
+        link: "https://zora.co/collect/0x907e2ea28394b337018c88994562520c57840b8e",
+        label: "WEIRD SENSATION",
+      },
+      {
+        link: "https://zora.co/collect/0x05BDd4A8A61AC5FA3A26476c8e05813504a71f1E/1",
+        label: "day-at-the-beach_2017.sMmry.injct",
+      },
+      {
+        link: "https://zora.co/collect/0x2577b607b9dd69365b2bdfbb40ff8dd651bf96a9",
+        label: "hithithit.png",
+      },
+      {
+        link: "https://zora.co/collect/0xb5898d79b1c129a6596a77f0d9ef5df5a2022119",
+        label:
+          "Torcida Jovem Fla Na Ilha Do Sol Com o Barulho Do Mar / Young Crowd Fla On The Island Of The Sun With The Noise Of The Sea",
+      },
+      {
+        link: "https://zora.co/collect/0x4c6b15e275c165394a01c379b74e9b5b9e338131/1",
+        label: "How to keep a secret (?) - attempt #1",
+      },
+      {
+        link: "https://zora.co/collect/0xe87f54b6df62c9fcc316ef199bffea7480727c8d/1",
+        label: "DOSSIER_SCAN_REMIX",
+      },
+    ],
+  },
+};
--- pages/web3/popnftnyc2023.vue
+++ pages/web3/popnftnyc2023.vue
@@ -1,1 +1,32 @@
-<template></template>
+<script setup lang="ts">
+import popnftnyc2023 from "@/content/web3/popnftnyc2023";
+
+const content = popnftnyc2023.en;
+</script>
+
+<template>
+  <div
+    class="w-full h-auto flex flex-col items-start justify-center px-6 md:px-16 gap-10 md:gap-16 pt-4 pb-28 md:pt-16 overflow-scroll"
+  >
+    <h1>{{ content.title }}</h1>
+    <iframe
+      :src="content.video"
+      class="w-full md:w-2/3 md:h-96 m-auto"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+    <div class="flex flex-col gap-6 items-start">
+      <a
+        v-for="piece in content.pieces"
+        :href="piece.link"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex items-start justify-center bg-white text-black px-2"
+      >
+        <span>_</span><span>{{ piece.label }}</span>
+      </a>
+    </div>
+  </div>
+</template>
