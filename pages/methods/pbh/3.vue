2f1305afd7906d1d6582ebd036ea02e2f3e05a16
tbizetde@gmail.com <tbizetde@gmail.com>
2025-06-16 19:09:46 -0300

    methods/pbh - room 5

--- pages/methods/pbh/3.vue
+++ pages/methods/pbh/3.vue
@@ -18,16 +18,24 @@ const content = computed(() => index[lang.value]);
       <div v-html="content.descriptionFirst" class="flex flex-col gap-6"></div>
 
       <div
-        class="flex flex-col mx-auto my-10 py-16 px-24 border-4 border-white gap-6"
+        class="flex flex-col mx-auto md:my-10 py-12 md:py-16 px-10 md:px-24 border-4 border-white gap-6 w-full md:w-auto"
       >
-        <div class="flex flex-row gap-3">
-          <span class="text-xl">{{ content.loginScreen.userPrompt }}</span>
-          <span class="text-xl">{{ content.loginScreen.userValue }}</span>
+        <div class="flex flex-col md:flex-row gap-3">
+          <span class="text-sm md:text-xl">{{
+            content.loginScreen.userPrompt
+          }}</span>
+          <span class="text-sm md:text-xl">{{
+            content.loginScreen.userValue
+          }}</span>
         </div>
 
-        <div class="flex flex-row gap-3">
-          <span class="text-xl">{{ content.loginScreen.passwordPrompt }}</span>
-          <span class="text-xl">{{ content.loginScreen.passwordValue }}</span>
+        <div class="flex flex-col md:flex-row gap-3">
+          <span class="text-sm md:text-xl">{{
+            content.loginScreen.passwordPrompt
+          }}</span>
+          <span class="text-sm md:text-xl">{{
+            content.loginScreen.passwordValue
+          }}</span>
         </div>
       </div>
 
