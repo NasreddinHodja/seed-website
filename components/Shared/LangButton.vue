631aa346759fd4d9094dad89fb1c8e719df9a9fc
tbizetde@gmail.com <tbizetde@gmail.com>
2025-06-16 21:47:06 -0300

    change layout

--- components/Shared/LangButton.vue
+++ components/Shared/LangButton.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-const lang = useCookie<"pt" | "en">("lang", { default: () => "en" });
+const lang = useCookie<"en" | "pt">("lang", { default: () => "en" });
 </script>
 
 <template>
   <div class="flex">
-    <div
-      class="font-bold px-3"
-      :class="{
-        'bg-white text-black': lang === 'pt',
-        'bg-black text-white': lang === 'en',
-      }"
-      @click="lang = 'pt'"
-    >
-      PT
-    </div>
     <div
       class="font-bold px-3"
       :class="{
@@ -23,6 +13,16 @@ const lang = useCookie<"pt" | "en">("lang", { default: () => "en" });
       @click="lang = 'en'"
     >
       EN
+    </div>
+    <div
+      class="font-bold px-3"
+      :class="{
+        'bg-white text-black': lang === 'pt',
+        'bg-black text-white': lang === 'en',
+      }"
+      @click="lang = 'pt'"
+    >
+      PT
     </div>
   </div>
 </template>
