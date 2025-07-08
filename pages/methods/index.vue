0cb8c5f9bbc113058d12106678988098952f7acf
tbizetde@gmail.com <tbizetde@gmail.com>
2025-07-08 03:17:14 -0300

    smoll layout fix

--- pages/methods/index.vue
+++ pages/methods/index.vue
@@ -6,7 +6,7 @@ const content = index.en;
 
 <template>
   <div
-    class="w-full flex flex-col items-start justify-center px-6 my-auto md:px-16 gap-10 md:gap-16 pt-4 pb-14 md:pt-32 overflow-auto"
+    class="w-full flex flex-col items-start justify-center px-6 my-auto md:px-16 gap-10 md:gap-16 pt-14 pb-28 md:pt-32 overflow-auto"
   >
     <div class="flex flex-col gap-10 md:gap-16">
       <div v-for="option in content.options">
--- pages/web3/index.vue
+++ pages/web3/index.vue
@@ -6,7 +6,7 @@ const content = index.en;
 
 <template>
   <div
-    class="w-full flex flex-col items-start justify-center px-6 md:px-16 gap-10 md:gap-16 pt-4 pb-14 md:pt-32 overflow-auto"
+    class="w-full flex flex-col items-start justify-center px-6 md:px-16 gap-10 md:gap-16 pt-14 pb-28 md:pt-32 overflow-auto"
   >
     <div v-for="option in content.options">
       <h1>
