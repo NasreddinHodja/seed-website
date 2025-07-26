2377576f206a038a89cf19b7fd85c888a4a651b0
tbizetde@gmail.com <tbizetde@gmail.com>
2025-07-26 03:31:34 -0300

    merch: finish refactor

--- pages/merch/002.vue
+++ pages/merch/002.vue
@@ -2,6 +2,7 @@
 import index from "~/content/merch/002";
 
 const content = index.en;
+// TODO: move video yt account
 // const videoId = extractYTVideoID(content.video);
 </script>
 
@@ -9,14 +10,14 @@ const content = index.en;
   <div
     class="flex flex-col w-screen h-auto gap-10 md:gap-16 px-6 md:px-16 pt-4 md:pt-16 pb-14 items-start md:items-start"
   >
-    <h1>{{ content.title }}</h1>
-    <SharedLink class="bg-white" :url="content.link.url">
-      <h2 class="text-black px-2">{{ content.link.label }}</h2>
-    </SharedLink>
+    <SharedPostHeader :title="content.title" :relatedLinks="[content.link]" />
+
+    <!-- TODO: move vide yt account -->
     <!-- <div v-if="videoId" class="m-auto flex flex-wrap items-end gap-4">
       <SharedYTLazyEmbed :video-id="videoId" class="w-full md:w-96" />
     </div> -->
-    <img class="m-auto w-full md:w-1/2" :src="content.flyer" />
+
+    <SharedFlyerImage class="m-auto w-full md:w-1/2" :src="content.flyer" />
 
     <div class="flex flex-row flex-wrap pt-10">
       <img
@@ -27,7 +28,7 @@ const content = index.en;
     </div>
 
     <div class="flex flex-col pt-10">
-      <img
+      <SharedFlyerImage
         v-for="picture in content.pictures"
         class="m-auto w-full md:w-1/2 pb-10"
         :src="picture"
@@ -49,9 +50,5 @@ const content = index.en;
         </div>
       </div>
     </div>
-    <!-- <div class="flex flex-wrap gap-1 justify-center">
-      <img :src="content.images[0]" class="w-full md:w-1/3" />
-      <img :src="content.images[1]" class="w-full md:w-1/3" />
-    </div> -->
   </div>
 </template>
--- pages/merch/index.vue
+++ pages/merch/index.vue
@@ -5,9 +5,7 @@ const content = index.en;
 </script>
 
 <template>
-  <div
-    class="w-full h-auto flex flex-col items-start justify-center px-6 md:px-16 gap-10 md:gap-16 pt-4 pb-14 md:pt-16 overflow-auto"
-  >
+  <LayoutsScrollablePost>
     <div v-for="option in content.options" class="flex w-full">
       <NuxtLink :to="option.url" class="block w-full">
         <div class="flex flex-col gap-4 w-full">
@@ -21,5 +19,5 @@ const content = index.en;
         </div>
       </NuxtLink>
     </div>
-  </div>
+  </LayoutsScrollablePost>
 </template>
